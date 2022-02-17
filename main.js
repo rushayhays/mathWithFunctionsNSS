66f12983d707a3f92e2cb3cc2527ee7379104b6e
rushayhays <rushayhays@outlook.com>
2022-02-17 13:41:42 -0600

    working functions, could use some clarifying on the process

---
+++ main.js
@@ -0,0 +1,12 @@
+function letsAdd(digit1, digit2) {
+    return digit1 + digit2;
+}
+
+function letsMultiply(num) {
+    return num * 2;
+}
+let thisSum = letsAdd(4, 14);
+
+let ourProduct = letsMultiply(thisSum);
+
+console.log(ourProduct)
